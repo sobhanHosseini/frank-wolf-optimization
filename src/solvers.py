8c0617b433948a8b922e70d5606d03e490f19c70
sobhan <hosseinisobhan@gmail.com>
2025-07-01 18:31:47 +0200

    a

--- src/solvers.py
+++ src/solvers.py
@@ -98,7 +98,6 @@ class FrankWolfe:
 
 class PairwiseFrankWolfe(FrankWolfe):
     def run(self, X0=None):
-        print("Running Pairwise Frank-Wolfe...")
         if X0 is not None:
             X = X0.copy()
         else:
